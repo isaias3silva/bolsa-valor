4de1f4cd033b834e60c1f6e6a0f66e298e569a01
isaias3silva <52718665+isaias3silva@users.noreply.github.com>
2021-10-20 17:01:21 -0300

    fist commit

---
+++ src/main/java/com/mmlogistica/bolsavalores/ProjetoBolsaDeValoresApplication.java
@@ -0,0 +1,13 @@
+package com.mmlogistica.bolsavalores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBolsaDeValoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBolsaDeValoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmlogistica/bolsavalores/ProjetoBolsaDeValoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmlogistica.bolsavalores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBolsaDeValoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
