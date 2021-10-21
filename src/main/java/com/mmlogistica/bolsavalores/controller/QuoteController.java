80d122ef19664f1aa8fb8817506a4e3907b72faa
isaias3silva <52718665+isaias3silva@users.noreply.github.com>
2021-10-21 14:41:21 -0300

    created class QuoteController and QuoteHandler

---
+++ src/main/java/com/mmlogistica/bolsavalores/controller/QuoteController.java
@@ -0,0 +1,21 @@
+package com.mmlogistica.bolsavalores.controller;
+
+import com.mmlogistica.bolsavalores.Entities.Quote;
+import com.mmlogistica.bolsavalores.repository.QuoteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path ="/quotes")
+public class QuoteController {
+
+    private final QuoteRepository quoteRepository;
+    @GetMapping
+    public Flux<Quote> getAll(){
+        return quoteRepository.findAll();
+    }
+}
---
+++ src/main/java/com/mmlogistica/bolsavalores/handler/QuoteHandler.java
@@ -0,0 +1,10 @@
+package com.mmlogistica.bolsavalores.handler;
+
+import com.mmlogistica.bolsavalores.repository.QuoteRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class QuoteHandler {
+    private final QuoteRepository quoteRepository;
+
+}
